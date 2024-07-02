1ef1e0298dac3ac1ed0449d6dfb39b87a7f86e70
Guido Fioravantti <guido.fioravantti@gmail.com>
2024-07-01 21:52:57 -0400

    I'm really trying to make neovim work here...

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,22 @@
+-- Bind ; to : in normal and visual mode
+vim.api.nvim_set_keymap("n", ";", ":", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", ";", ":", { noremap = true, silent = true })
+
+-- Bind <C-h> to <C-w>h in normal and terminal mode
+vim.api.nvim_set_keymap("n", "<C-h>", "<C-w>h", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("t", "<C-h>", "<C-w>h", { noremap = true, silent = true })
+
+-- Bind <C-j> to <C-w>j in normal and terminal mode
+vim.api.nvim_set_keymap("n", "<C-j>", "<C-w>j", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("t", "<C-j>", "<C-w>j", { noremap = true, silent = true })
+
+-- Bind <C-k> to <C-w>k in normal and terminal mode
+vim.api.nvim_set_keymap("n", "<C-k>", "<C-w>k", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("t", "<C-k>", "<C-w>k", { noremap = true, silent = true })
+
+-- Bind <C-l> to <C-w>l in normal and terminal mode
+vim.api.nvim_set_keymap("n", "<C-l>", "<C-w>l", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("t", "<C-l>", "<C-w>l", { noremap = true, silent = true })
+
+-- Bind <C-n> to <C-\><C-n> in terminal mode
+vim.api.nvim_set_keymap("t", "<C-n>", "<C-\\><C-n>", { noremap = true, silent = true })
---
+++ lua/config/opt.lua
@@ -0,0 +1,23 @@
+-- Set up tabstops
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 0
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.smarttab = true
+
+-- Don't highlight search
+vim.opt.hlsearch = false
+
+-- Turn syntax highlighting off
+vim.opt.syntax = "off"
+
+-- Set relative number
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- Set color column to 88
+vim.opt.colorcolumn = "88"
+
+-- Reverse split directions
+vim.opt.splitright = true
+vim.opt.splitbelow = true
