31f709bbf17090c9fc5bc68f2655dd5f970a6b39
Guido Fioravantti <guido.fioravantti@gmail.com>
2024-07-03 10:21:14 -0400

    Consolidate fzf and fzf.vim

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -1,4 +1,15 @@
 return {
-    "junegunn/fzf",
-    build = "./install --bin"
+    {
+        "junegunn/fzf",
+        build = "./install --bin"
+    },
+    {
+        "junegunn/fzf.vim",
+        keys = {
+            { "<C-p>", ":Files<CR>" },
+            { "<C-n>", ":Buffers<CR>" },
+            { "<C-f>", ":History:<CR>" },
+            { "<C-g>", ":GFiles<CR>" },
+        },
+    },
 }
--- lua/plugins/fzf.vim.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "junegunn/fzf.vim",
-    keys = {
-        { "<C-p>", ":Files<CR>" },
-        { "<C-n>", ":Buffers<CR>" },
-        { "<C-f>", ":History:<CR>" },
-        { "<C-g>", ":GFiles<CR>" },
-    },
-}
