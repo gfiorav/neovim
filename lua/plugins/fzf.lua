cd73259d9eeebe5214a11a8e0b93a47ba58804c0
Guido Fioravantti <guido.fioravantti@gmail.com>
2026-01-12 22:53:01 -0500

    remap buffer search to C-b to avoid C-n (normal) shortcut

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -7,7 +7,7 @@ return {
         "junegunn/fzf.vim",
         keys = {
             { "<C-p>", ":Files<CR>" },
-            { "<C-n>", ":Buffers<CR>" },
+            { "<C-b>", ":Buffers<CR>" },
             { "<C-f>", ":History:<CR>" },
             { "<C-g>", ":GFiles<CR>" },
         },
